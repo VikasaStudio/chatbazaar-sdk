643874cf53f3a96d9a81d5cc2ff39242c22d1a9f
Keshav Sharma <sharmakeshav15157@gmail.com>
2023-08-31 03:19:40 +0530

    new ubild

--- lib/cjs/services/store-service.d.ts
+++ lib/cjs/services/store-service.d.ts
@@ -1,10 +1,19 @@
+import { VendorSchema } from "../interfaces/repositories/VendorRepositoryInterfaces";
 import { CreateVendorRequest } from "../interfaces/services/StoreServiceInterfaces";
 import { IServiceOptions, Service } from "./service";
 export declare class StoreService extends Service {
     constructor(serviceBaseUrl: string, apiKey: string);
-    getStoreById(vendorId: string, options?: IServiceOptions | {}): Promise<import("./service").ServiceApiResponse<any>>;
-    getStoreByStoreCode(storeCode: string, options?: IServiceOptions | {}): Promise<import("./service").ServiceApiResponse<any>>;
-    createStore(payload: CreateVendorRequest, options?: IServiceOptions | {}): Promise<import("./service").ServiceApiResponse<any>>;
+    getStoreById(vendorId: string, options?: IServiceOptions | {}): Promise<import("./service").ServiceApiResponse<{
+        data: VendorSchema;
+    }>>;
+    getStoreByStoreCode(storeCode: string, options?: IServiceOptions | {}): Promise<import("./service").ServiceApiResponse<{
+        data: VendorSchema;
+    }>>;
+    createStore(payload: CreateVendorRequest, options?: IServiceOptions | {}): Promise<import("./service").ServiceApiResponse<{
+        refreshToken: string;
+        accessToken: string;
+        vendorId: string;
+    }>>;
     loginVendor(payload: {
         identifierType: `EMAIL` | "STORECODE";
         identifierValue: string;
